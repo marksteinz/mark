c458d18c3019dada35f7d2ecb773d1d3c62994d2
marksteinz <58270819+marksteinz@users.noreply.github.com>
2020-01-14 23:47:07 +0300

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+	public static void main(String[] args) {
+		byte cpuCountCore = 6;
+		short cpuSpeed = 2800;
+		int ramMemory = 16;
+		long hddMemory = 512;
+		float cpuClockRate = 2200.5f;
+		double ramClockRate = 2008.8d;
+		char gpuMemory = 8;
+		boolean pc;
+
+	System.out.println("cpuCountCore");
+	System.out.println("cpuSpeed");
+	System.out.println("ramMemory");
+	System.out.println("hddMemory");
+	System.out.println("cpuClockRate");
+	System.out.println("ramClockRate");
+	System.out.println("gpuMemory");
+	System.out.println("pc");
+	}
+}
