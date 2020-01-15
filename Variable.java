a62ed711f9d22f2f07da72e07ab99faef61376ca
marksteinz <58270819+marksteinz@users.noreply.github.com>
2020-01-16 00:28:33 +0300

    Update Variable.java

--- Variable.java
+++ Variable.java
@@ -7,15 +7,15 @@ public class Variable {
 		float cpuClockRate = 2200.5f;
 		double ramClockRate = 2008.8d;
 		char gpuMemory = 8;
-		boolean pc;
+		boolean isPc = true;
 
-	System.out.println("cpuCountCore");
-	System.out.println("cpuSpeed");
-	System.out.println("ramMemory");
-	System.out.println("hddMemory");
-	System.out.println("cpuClockRate");
-	System.out.println("ramClockRate");
-	System.out.println("gpuMemory");
-	System.out.println("pc");
+		System.out.println("cpuCountCore");
+		System.out.println("cpuSpeed");
+		System.out.println("ramMemory");
+		System.out.println("hddMemory");
+		System.out.println("cpuClockRate");
+		System.out.println("ramClockRate");
+		System.out.println("gpuMemory");
+		System.out.println("pc");
 	}
 }
