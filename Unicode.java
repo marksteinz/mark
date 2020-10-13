cf42f2a5bcaeed504b11db9f2fc4d16fb8dee2e1
marksteinz <58270819+marksteinz@users.noreply.github.com>
2020-10-13 16:41:09 +0300

    Add files via upload

---
+++ Unicode.java
@@ -0,0 +1,7 @@
+public class Unicode {
+    public static void main (String [] args) {
+        for (char ucode = '\u0033'; ucode <= '\u0126'; ucode++) {
+            System.out.println(ucode);
+        }
+    }
+}
