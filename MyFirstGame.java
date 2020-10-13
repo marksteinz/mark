29576bad88264d6fa399859f73287e985833eb28
marksteinz <58270819+marksteinz@users.noreply.github.com>
2020-10-13 16:40:23 +0300

    Add files via upload

---
+++ MyFirstGame.java
@@ -0,0 +1,15 @@
+public class MyFirstGame {
+    public static void main (String [] args) {
+        int computer = 73;
+        int my = 73;
+
+        if ( my < computer) {
+            System.out.println("Введенное вами число меньше того, что загадал компьютер");
+        } else if ( my > computer) {
+            System.out.println("Введенное вами число больше того, что загадал компьютер");
+        } else {
+            System.out.println("Вы угадали!");
+        }
+
+    }
+}
